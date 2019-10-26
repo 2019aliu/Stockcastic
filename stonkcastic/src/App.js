042ffcea784180ff2881261e4fdd49808d60845c
EthanGordon <egordon37@gatech.edu>
2019-10-25 23:48:28 -0400

    added sentiment basic function. run npm install --save @google-cloud/language to build

--- stonkcastic/src/App.js
+++ stonkcastic/src/App.js
@@ -1,19 +1,45 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-import LoginPage from "./components/login-page.component.js";
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Link, Route} from 'react-router-dom';
+
+import LoginPage from './components/login-page.component.js';
 
 class App extends Component {
   render() {
     return(
       <Router>
-        <div className="container">
+        <div className='container'>
           <h2>MERN-Stack Todo App</h2>
         </div>
       </Router>
     )
   }
 }
+var happyString = 'Yay! this is so great!'
+async function do_sentiment() {
+  // Imports the Google Cloud client library
+    const language = require('@google-cloud/language');
 
-export default App;
+        // Instantiates a client
+        const client = new language.LanguageServiceClient();
+
+        // The text to analyze
+        const text = happyString;
+
+        const document = {
+          content: text,
+          type: 'PLAIN_TEXT',
+        };
+
+        // Detects the sentiment of the text
+        const [result] = await client.analyzeSentiment({document: document});
+        const sentiment = result.documentSentiment;
+
+        console.log(`Text: ${text}`);
+        console.log(`Sentiment score: ${sentiment.score}`);
+        console.log(`Sentiment magnitude: ${sentiment.magnitude}`);
+  }
+  do_sentiment();
+
+  export default App;
