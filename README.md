7c88a4dc820138ea9ac1b07b2420a88f190c93e9
ceelo777 <lochristopher9@gmail.com>
2019-10-25 23:39:25 -0400

    Added a component and edited App.js to fit the tutorial

--- README.md
+++
@@ -1,2 +0,0 @@
-# Stonkcastic
-An application for sentimental analysis of stocks. Stonks are great. Made for HackGT 6.
---
+++ stonkcastic/src/components/login-page.component.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+export default class LoginPage extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+}
