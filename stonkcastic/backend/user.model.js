8bb8eb5d52755d67a388e23a32dee56e975687c0
ceelo777 <lochristopher9@gmail.com>
2019-10-26 13:07:10 -0400

    Added the backend to the MERN server

--- stonkcastic/backend/user.model.js
+++ stonkcastic/backend/user.model.js
@@ -1,15 +1,13 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const Stock = require('./stock.model');
 
-// the tutorial says to use {type: String}
-// but as long as we don't have anything else
-// itll be ok
 let User = new Schema({
-    user_name: {type: String},
-    user_pass: {type: String},
-    user_stocks: {type: [Stock]}
+    username: {
+        type: String
+    },
+    password: {
+        type: String
+    }
 });
 
-// doesn't actually do anything lel
-module.exports = mongoose.model('UserScheme', User);
+module.exports = mongoose.model('User', User);
