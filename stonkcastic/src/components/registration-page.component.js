e1ef2f70ac4163f873934f06aa3ed5ddcd0dc174
ceelo777 <lochristopher9@gmail.com>
2019-10-27 01:35:02 -0400

    Added Login Page, edited Registration page, created User Profile Component

--- stonkcastic/src/components/registration-page.component.js
+++ stonkcastic/src/components/registration-page.component.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-export default class LoginPage extends Component {
+export default class RegistrationPage extends Component {
 
     constructor(props) {
         super(props);
 
         this.onChangeUsername = this.onChangeUsername.bind(this);
         this.onChangePassword = this.onChangePassword.bind(this);
+        this.onChangeStock = this.onChangeStock.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
 
         this.state = {
             username: '',
             password: '',
+            newStock: '',            
+            stocks: []
         }
     }
     onChangeUsername(e) {
@@ -27,6 +30,12 @@ export default class LoginPage extends Component {
         })
     }
 
+    onChangeStock(e) {
+        this.setState({
+            newStock: e.target.value
+        })
+    }
+
     onSubmit(e) {
         e.preventDefault();
         this.state.stocks.push(this.state.newStock);
@@ -34,15 +43,17 @@ export default class LoginPage extends Component {
         const newUser = {
             username: this.state.username,
             password: this.state.password,
+            stocks: this.state.stocks
         }
 
         console.log('Form submitted:');
         console.log('Username: ${this.state.username}');
         console.log('Password: ${this.state.password}');
+        console.log('Stocks: ${this.state.stocks}');
 
         console.log(newUser);
 
-        axios.post('http://localhost:8000/users/auth', newUser)
+        axios.post('http://localhost:8000/users/add', newUser)
             .then(res => console.log(res.data))
             .catch(error => {
                 console.log(error.response)
@@ -51,13 +62,15 @@ export default class LoginPage extends Component {
         this.setState({
             username: '',
             password: '',
+            newStock: '',
+            stocks: [],
         })
     }
 
     render() {
         return (
             <div style={{ marginTop: 10 }}>
-                <h3>Login Page</h3>
+                <h3>Registration Page</h3>
                 <form onSubmit={this.onSubmit}>
                     <div className="form-group">
                         <label>Username: </label>
@@ -74,6 +87,15 @@ export default class LoginPage extends Component {
                             className="form-control"
                             value={this.state.password}
                             onChange={this.onChangePassword}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Stock: </label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            value={this.state.newStock}
+                            onChange={this.onChangeStock}
                         />
                     </div>
                     <div className="form-group">
